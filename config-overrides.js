cf9cb7cf0258f90019b957b8c4c80d071dbd9ad9
boweryjg <boweryjg@gmail.com>
2025-06-07 04:39:10 -0400

    Fix config-overrides.js - remove unused imports

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,3 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-
 module.exports = function override(config, env) {
   // Return the default config without modifications
   return config;
