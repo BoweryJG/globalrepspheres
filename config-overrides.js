935b821022bf4da686a3342d8c60eb07e828ad79
boweryjg <boweryjg@gmail.com>
2025-06-06 22:11:29 -0400

    Remove standalone chatbot page - keep only floating launcher
    - Delete chatbot.html, chatbot.js, ChatbotPage.js
    - Remove webpack config for separate chatbot entry
    - Chatbot now ONLY accessible via floating button
    - Prevents full-page chatbot overlay issue

--- config-overrides.js
+++ config-overrides.js
@@ -2,48 +2,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = function override(config, env) {
-  // Add chatbot entry point
-  config.entry = {
-    main: config.entry,
-    chatbot: path.resolve(__dirname, 'src/chatbot.js')
-  };
-
-  // Update output to use proper naming
-  config.output.filename = 'static/js/[name].[contenthash:8].js';
-
-  // Add HtmlWebpackPlugin for chatbot
-  config.plugins.push(
-    new HtmlWebpackPlugin({
-      inject: true,
-      chunks: ['chatbot'],
-      template: path.resolve(__dirname, 'public/chatbot.html'),
-      filename: 'chatbot.html',
-      ...(env === 'production'
-        ? {
-            minify: {
-              removeComments: true,
-              collapseWhitespace: true,
-              removeRedundantAttributes: true,
-              useShortDoctype: true,
-              removeEmptyAttributes: true,
-              removeStyleLinkTypeAttributes: true,
-              keepClosingSlash: true,
-              minifyJS: true,
-              minifyCSS: true,
-              minifyURLs: true,
-            },
-          }
-        : undefined),
-    })
-  );
-
-  // Update the main HTML plugin to only include main chunks
-  const htmlPlugin = config.plugins.find(
-    plugin => plugin.constructor.name === 'HtmlWebpackPlugin'
-  );
-  if (htmlPlugin) {
-    htmlPlugin.userOptions.chunks = ['main'];
-  }
-
+  // Return the default config without modifications
   return config;
 };
